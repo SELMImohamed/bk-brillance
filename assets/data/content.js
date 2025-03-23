99ebde4fddd0aead06c97cb97e5ed27370686dd6
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2025-03-23 05:00:11 +0100

    🔓 Autorise l'accès public à content.js via /assets

--- assets/data/content.js
+++ assets/data/content.js
@@ -1,6 +1,6 @@
 const data = {
   "header": {
-    "logo": "test 4H39",
+    "logo": "test 4H3",
     "cta": "Devis gratuit",
     "menu": [
       "Services",
@@ -10,7 +10,7 @@ const data = {
     ]
   },
   "footer": {
-    "company_name": "test 4H39",
+    "company_name": "test 4H3",
     "company_desc": "Votre partenaire propreté professionnelll",
     "contact": {
       "phone": "01 23 45 67 89",
