b0500b1916284a8c47c2ba6806b38362c7e73a31
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2025-04-19 18:13:08 +0200

    test

--- server/assets/data/content.js
+++ server/assets/data/content.js
@@ -91,3 +91,12 @@ const data = {
     }
   ]
 };
+
+async function fetchContentData() {
+  const res = await fetch(`/assets/data/content.js?ts=${Date.now()}`);
+  const text = await res.text();
+
+  // ⚠️ transforme la string JS en objet JavaScript réel
+  const data = eval(text.replace("const data = ", "").replace(/;$/, ""));
+  return data;
+}
